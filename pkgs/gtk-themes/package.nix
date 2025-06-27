020b3767cdaef2dead62d25c800a0bdd4a52d6c2
maotseantonio <thetzinantonio@gmail.com>
2025-06-27 14:08:14 +0630

    update-gtk-themes

--- pkgs/gtk-themes/package.nix
+++ pkgs/gtk-themes/package.nix
@@ -1,28 +1,36 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation {
-  pname = "maotseantonio-gtk-themes";
-  version = "1.0";
-
+let
   src = pkgs.fetchFromGitHub {
-       owner = "maotseantonio";
-       repo = "gtk-themes";
-       rev = "a627da499595593af99366203301b363b2dd6576";
-       hash = "sha256-2ThCa0gkjs7jFt8BXMJOpXhlNpzKVsx0PJIkgm5Z+eE=";
+    owner = "maotseantonio";
+    repo = "gtk-themes";
+    rev = "a627da499595593af99366203301b363b2dd6576";
+    hash = "sha256-2ThCa0gkjs7jFt8BXMJOpXhlNpzKVsx0PJIkgm5Z+eE=";
   };
- 
-  installPhase = ''
-    mkdir -p $out/share/themes
-    for theme in CatppuccinMocha-zk OxoCarbon-zk TokyoNight-zk; do
-      cp -r "$theme" "$out/share/themes/"
-    done
-  '';
 
-  meta = with pkgs.lib; {
-    description = "Custom GTK themes: CatppuccinMocha-zk, OxoCarbon-zk, TokyoNight-zk";
-    homepage = "https://github.com/maotseantonio/gtk-themes";
-    license = licenses.mit;
-    platforms = platforms.linux;
+  mkTheme = themeName: pkgs.stdenv.mkDerivation {
+    pname = "maotseantonio-${themeName}";
+    version = "1.0";
+    inherit src;
+
+    installPhase = ''
+      mkdir -p $out/share/themes
+      cp -r "$src/${themeName}" "$out/share/themes/${themeName}"
+    '';
+
+    meta = with pkgs.lib; {
+      description = "GTK theme ${themeName} from maotseantonio/gtk-themes";
+      homepage = "https://github.com/maotseantonio/gtk-themes";
+      license = licenses.mit;
+      platforms = platforms.linux;
+    };
   };
+
+in
+{
+  CatppuccinMocha-zk = mkTheme "CatppuccinMocha-zk";
+  OxoCarbon-zk = mkTheme "OxoCarbon-zk";
+  TokyoNight-zk = mkTheme "TokyoNight-zk";
 }
+
 
